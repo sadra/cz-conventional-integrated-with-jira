16cc663bf2ab71fcf48a81dbbaabac872c9a6f5a
Sadra Amlashi <amlashi.sadra@gmail.com>
2022-07-11 15:23:10 +0200

    feat: Add integration with Jira

--- cz_int_jira.py
+++ cz_int_jira.py
@@ -8,6 +8,15 @@ from commitizen.cz.conventional_commits import ConventionalCommitsCz
 from commitizen.defaults import Questions
 from commitizen import defaults
 
+def parse_jira_ref(text):
+    if not text:
+        return ""
+
+    matched = re.match("[A-Z]+-\d+", text)
+    if not bool(matched):
+        raise exceptions.AnswerRequiredError("Invalid JIRA reference.")
+
+    return text
 
 class CzIntegeratedJira(ConventionalCommitsCz):
     def __init__(self, config: BaseConfig):
@@ -30,11 +39,47 @@ class CzIntegeratedJira(ConventionalCommitsCz):
 
     def questions(self) -> Questions:
         questions = super().questions()
+        issue_key = None
+
+        if git.is_git_project():
+            branch_name = cmd.run('git branch --show-current')
+            if branch_name.out:
+                m = re.search('[A-Z]+-\d+', branch_name.out)
+                issue_key = m[0] if m is not None else None
+
+        if issue_key:
+            questions.append({
+                "type": "input",
+                "name": "jira_ref",
+                "default": issue_key,
+                "message": (
+                    "Jira reference\n"
+                ),
+                "filter": parse_jira_ref
+            })
+        else:
+            questions.append({
+                "type": "input",
+                "name": "jira_ref",
+                "message": (
+                    f"Jira reference (press [enter] to skip)\n"
+                ),
+                "filter": parse_jira_ref
+            })
 
         return questions
 
     def message(self, answers: dict) -> str:
         message = super().message(answers)
+
+        footer = answers["footer"]
+        jira_ref = answers["jira_ref"]
+        if jira_ref:
+            if footer:
+                message = f"{message}\nJira-ref: {jira_ref}"
+            else:
+                message = f"{message}\n\nJira-ref: {jira_ref}"
+
         return message
 
     def example(self) -> str:
